84bd6bcbacb1dac8c7b810f3c6b0f597a8dbb018
Nahro <nahropro@gmail.com>
2023-07-22 04:42:41 +0300

    Perform reload-after-save in trigger instead of controller

--- ShiftIdentity.Dashboard.AspNetCore/Extentsions/IMvcBuilderExtensions.cs
+++ ShiftIdentity.Dashboard.AspNetCore/Extentsions/IMvcBuilderExtensions.cs
@@ -6,13 +6,38 @@ using ShiftSoftware.ShiftIdentity.AspNetCore.Services.Interfaces;
 using ShiftSoftware.ShiftIdentity.Dashboard.AspNetCore.Data;
 using ShiftSoftware.ShiftIdentity.AspNetCore.IRepositories;
 using ShiftSoftware.ShiftIdentity.AspNetCore;
+using ShiftSoftware.ShiftEntity.Core;
+using System.Reflection;
 
 namespace ShiftSoftware.ShiftIdentity.Dashboard.AspNetCore.Extentsions;
 
 public static class IMvcBuilderExtensions
 {
+    private static IMvcBuilder RegisterIShiftEntityFind(this IMvcBuilder builder)
+    {
+        Assembly repositoryAssembly = Assembly.GetExecutingAssembly(); // Adjust this as needed
+
+        // Find all types in the assembly that implement IRepository<>
+        var repositoryTypes = repositoryAssembly.GetTypes()
+            .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IShiftEntityFind<>)));
+
+        // Register each IRepository<> implementation with its corresponding interface
+        foreach (var repositoryType in repositoryTypes)
+        {
+            var interfaceType = repositoryType.GetInterfaces().FirstOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IShiftEntityFind<>));
+            if (interfaceType != null)
+            {
+                builder.Services.AddScoped(interfaceType, repositoryType);
+            }
+        }
+
+        return builder;
+    }
+
     public static IMvcBuilder AddShiftIdentityDashboard<TDbContext>(this IMvcBuilder builder, ShiftIdentityConfiguration shiftIdentityConfiguration) where TDbContext : ShiftIdentityDB
     {
+        builder.RegisterIShiftEntityFind();
+
         builder.Services.TryAddSingleton(shiftIdentityConfiguration);
 
         builder.Services.AddSingleton<AuthCodeStoreService>();
