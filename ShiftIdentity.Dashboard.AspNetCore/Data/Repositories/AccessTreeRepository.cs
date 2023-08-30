dfc262777db450d3e26b58fd70d54a1027fad845
Aza Asim <azaasim@gmail.com>
2023-08-30 14:42:19 +0300

    Use the new default repository for AccessTree

--- ShiftIdentity.Dashboard.AspNetCore/Data/Repositories/AccessTreeRepository.cs
+++ ShiftIdentity.Dashboard.AspNetCore/Data/Repositories/AccessTreeRepository.cs
@@ -1,79 +1,40 @@
 ﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
 using ShiftSoftware.ShiftEntity.EFCore;
 using ShiftSoftware.ShiftEntity.Core;
-using ShiftSoftware.ShiftEntity.Model;
 using ShiftSoftware.ShiftIdentity.AspNetCore.Entities;
 using ShiftSoftware.ShiftIdentity.Core.DTOs.AccessTree;
 using ShiftSoftware.TypeAuth.AspNetCore.Services;
+using Microsoft.EntityFrameworkCore;
+using ShiftSoftware.ShiftEntity.Model;
 using ShiftSoftware.TypeAuth.Core;
 using System.Net;
 
 namespace ShiftSoftware.ShiftIdentity.Dashboard.AspNetCore.Data.Repositories;
 
-public class AccessTreeRepository : ShiftRepository<ShiftIdentityDB, AccessTree>,
+public class AccessTreeRepository :
+    ShiftRepository<ShiftIdentityDB, AccessTree, AccessTreeDTO, AccessTreeDTO, AccessTreeDTO>,
     IShiftRepositoryAsync<AccessTree, AccessTreeDTO, AccessTreeDTO>
 {
-    private readonly ShiftIdentityDB db;
     private readonly TypeAuthService typeAuthService;
     public AccessTreeRepository(ShiftIdentityDB db, TypeAuthService typeAuthService, IMapper mapper) : base(db, db.AccessTrees, mapper)
     {
-        this.db = db;
         this.typeAuthService = typeAuthService;
     }
-    public async ValueTask<AccessTree> CreateAsync(AccessTreeDTO dto, long? userId = null)
+
+    public override async ValueTask<AccessTree> UpsertAsync(AccessTree entity, AccessTreeDTO dto, ActionTypes actionType, long? userId = null)
     {
+        long id = 0;
+
+        if (actionType == ActionTypes.Update)
+            id = dto.ID!.ToLong();
+
         //Check if the access-tree-name in the same scope is duplicate
-        if (await db.AccessTrees.AnyAsync(x => x.Name.ToLower() == dto.Name.ToLower()))
-            throw new ShiftEntityException(new Message("Duplicate", $"the scope name {dto.Name} is exists for this scope"));
+        if (await db.AccessTrees.AnyAsync(x => x.Name.ToLower() == dto.Name.ToLower() && x.ID != id))
+            throw new ShiftEntityException(new Message("Duplicate", $"the access tree name ({dto.Name}) already exists."));
 
-        var entity = new AccessTree().CreateShiftEntity(userId);
+        if (entity.BuiltIn)
+            throw new ShiftEntityException(new Message("Error", "Built-In Data can't be modified."), (int)HttpStatusCode.Forbidden);
 
-        AssignValues(dto, entity);
-
-        return entity;
-    }
-    
-    public ValueTask<AccessTree> DeleteAsync(AccessTree entity, long? userId = null)
-    {
-        entity.DeleteShiftEntity(userId);
-        return new ValueTask<AccessTree>(entity);
-    }
-
-    public async Task<AccessTree> FindAsync(long id, DateTime? asOf = null)
-    {
-        return await base.FindAsync
-            (id, asOf);
-    }
-
-    public IQueryable<AccessTreeDTO> OdataList(bool showDeletedRows = false)
-    {
-        IQueryable<AccessTree> trees= GetIQueryable(showDeletedRows).AsNoTracking();
-
-        return mapper.ProjectTo<AccessTreeDTO>(trees);
-    }
-
-    public async ValueTask<AccessTree> UpdateAsync(AccessTree entity, AccessTreeDTO dto, long? userId = null)
-    {
-        //Check if the access-tree-name in the same scope is duplicate
-        if (dto.Name.ToLower() != entity.Name.ToLower())
-            if (await db.AccessTrees.AnyAsync(x => x.Name.ToLower() == dto.Name.ToLower()))
-                throw new ShiftEntityException(new Message("Duplicate", $"the scope name {dto.Name} is exists"));
-
-        entity.UpdateShiftEntity(userId);
-
-        AssignValues(dto, entity);
-
-        return entity;
-    }
-
-    public ValueTask<AccessTreeDTO> ViewAsync(AccessTree entity)
-    {
-        return new ValueTask<AccessTreeDTO>(mapper.Map<AccessTreeDTO>(entity));
-    }
-
-    private void AssignValues(AccessTreeDTO dto, AccessTree entity)
-    {
         var typeAuthContextBuilder_Producer = new TypeAuthContextBuilder();
         var typeAuthContextBuilder_Preserver = new TypeAuthContextBuilder();
 
@@ -88,7 +49,7 @@ public class AccessTreeRepository : ShiftRepository<ShiftIdentityDB, AccessTree>
 
         typeAuthContextBuilder_Producer.AddAccessTree(dto.Tree);
 
-        if (entity.ID != default)
+        if (actionType == ActionTypes.Update)
         {
             typeAuthContextBuilder_Preserver.AddAccessTree(entity.Tree);
             typeAuth_Preserver = typeAuthContextBuilder_Preserver.Build();
@@ -100,7 +61,6 @@ public class AccessTreeRepository : ShiftRepository<ShiftIdentityDB, AccessTree>
 
         entity.Name = dto.Name;
 
-        if (entity.BuiltIn)
-            throw new ShiftEntityException(new Message("Error", "Built-In Data can't be modified."), (int)HttpStatusCode.Forbidden);
+        return entity;
     }
 }
