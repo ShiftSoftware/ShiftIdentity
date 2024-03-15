64c9d34dd38c79689e20596cca19aff6ef3701a3
Aza Asim <aza@shift.software>
2024-03-15 12:51:56 +0300

    Use LocalizedText for Region name

--- ShiftIdentity.Core/DTOs/Region/RegionListDTO.cs
+++ ShiftIdentity.Core/DTOs/Region/RegionListDTO.cs
@@ -2,6 +2,7 @@
 using ShiftSoftware.ShiftEntity.Model;
 using ShiftSoftware.ShiftEntity.Model.Dtos;
 using ShiftSoftware.ShiftEntity.Model.HashIds;
+using System.Text.Json.Serialization;
 
 namespace ShiftSoftware.ShiftIdentity.Core.DTOs.Region;
 
@@ -11,6 +12,7 @@ public class RegionListDTO : ShiftEntityListDTO
     [RegionHashIdConverter]
     public override string? ID { get; set; }
 
+    [JsonConverter(typeof(LocalizedTextJsonConverter))]
     public string Name { get; set; } = default!;
     public string? ExternalId { get; set; } = default!;
     public string? ShortCode { get; set; }
