fb13b8e7a9e164df8c4be472184588bc3d5f753a
Aza Asim <aza@shift.software>
2024-03-27 14:30:35 +0300

    Brand Name Should be Localizable

--- ShiftIdentity.Core/DTOs/Brand/BrandListDTO.cs
+++ ShiftIdentity.Core/DTOs/Brand/BrandListDTO.cs
@@ -1,6 +1,7 @@
 ﻿using ShiftSoftware.ShiftEntity.Model;
 using ShiftSoftware.ShiftEntity.Model.Dtos;
 using ShiftSoftware.ShiftEntity.Model.HashIds;
+using System.Text.Json.Serialization;
 
 namespace ShiftSoftware.ShiftIdentity.Core.DTOs.Brand;
 
@@ -9,5 +10,7 @@ public class BrandListDTO : ShiftEntityListDTO
 {
     [BrandHashIdConverter]
     public override string? ID { get; set; }
+
+    [JsonConverter(typeof(LocalizedTextJsonConverter))]
     public string Name { get; set; } = default!;
 }
