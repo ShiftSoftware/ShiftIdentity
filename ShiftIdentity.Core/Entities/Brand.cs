9d87d84fa48efd645584b754113e38a90bee0fd9
Aza Asim <aza@shift.software>
2024-04-11 21:01:02 +0300

    #86ep33fpy: Brand Flag: Mark Entities as having Brand Flag

--- ShiftIdentity.Core/Entities/Brand.cs
+++ ShiftIdentity.Core/Entities/Brand.cs
@@ -1,10 +1,12 @@
 ﻿using ShiftSoftware.ShiftEntity.Core;
+using ShiftSoftware.ShiftEntity.Model;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShiftSoftware.ShiftIdentity.Core.Entities;
 
 [TemporalShiftEntity]
 [Table("Brands", Schema = "ShiftIdentity")]
+[ShiftEntityKeyAndName(nameof(ID), nameof(Name))]
 public class Brand : ShiftEntity<Brand>
 {
     public string Name { get; set; } = default!;
