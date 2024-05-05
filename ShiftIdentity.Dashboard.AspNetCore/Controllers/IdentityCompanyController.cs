b078385edc860a4285ce283c0424306c09f35a58
Aza Asim <aza@shift.software>
2024-05-05 21:32:39 +0300

    Companies & Branches Controller: Data Level Access

--- ShiftIdentity.Dashboard.AspNetCore/Controllers/IdentityCompanyController.cs
+++ ShiftIdentity.Dashboard.AspNetCore/Controllers/IdentityCompanyController.cs
@@ -1,16 +1,22 @@
-﻿using ShiftSoftware.ShiftEntity.Web;
-using ShiftSoftware.ShiftIdentity.Core.DTOs.Company;
-using ShiftSoftware.ShiftIdentity.Data.Repositories;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using ShiftSoftware.ShiftEntity.Web;
 using ShiftSoftware.ShiftIdentity.Core;
+using ShiftSoftware.ShiftIdentity.Core.DTOs.Company;
 using ShiftSoftware.ShiftIdentity.Core.Entities;
+using ShiftSoftware.ShiftIdentity.Data.Repositories;
 
 namespace ShiftSoftware.ShiftIdentity.Dashboard.AspNetCore.Controllers;
 
 [Route("api/[controller]")]
 public class IdentityCompanyController : ShiftEntitySecureControllerAsync<CompanyRepository, Company, CompanyListDTO, CompanyDTO>
 {
-    public IdentityCompanyController() : base(ShiftIdentityActions.Companies)
+    public IdentityCompanyController() : base(ShiftIdentityActions.Companies,
+        x => x
+        .FilterBy(x => x.ID, ShiftIdentityActions.DataLevelAccess.Companies)
+        .DecodeHashId<CompanyDTO>()
+        .IncludeCreatedByCurrentUser(x => x.CreatedByUserID)
+        .IncludeSelfItems(ShiftEntity.Core.Constants.CompanyIdClaim)
+    )
     {
     }
 }
