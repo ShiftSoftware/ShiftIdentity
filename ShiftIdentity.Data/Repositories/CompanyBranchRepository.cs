f51963927f7046e14297abf2ce9cff2efb721b27
Seniister <sarmadahmad549@gmail.com>
2026-01-17 07:33:40 +0300

    #86ev6zju8: CompanyBranchRepository: Assign new Description and Website URL propertie in Upsert

--- ShiftIdentity.Data/Repositories/CompanyBranchRepository.cs
+++ ShiftIdentity.Data/Repositories/CompanyBranchRepository.cs
@@ -151,6 +151,8 @@ namespace ShiftSoftware.ShiftIdentity.Data.Repositories
             entity.WorkingDays = dto.WorkingDays;
             entity.DisplayName = dto.DisplayName;
             entity.DisplayOrder = dto.DisplayOrder;
+            entity.Description = dto.Description;
+            entity.WebsiteURL = dto.WebsiteURL;
             entity.PublishTargets = dto.PublishTargets?.ToList();
             //ef core may not set the entity state as Modified if only the collections are changed (CompanyBranchDepartments, CompanyBranchServices)
             if (actionType == ActionTypes.Update)
